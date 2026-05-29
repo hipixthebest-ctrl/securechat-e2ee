fdbc2cf7d28631b7345f3bc05d247ca98a36d08c
hipixthebest-ctrl <hipixthebest@gmail.com>
2026-05-30 00:30:41 +0300

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, {
+const io = socketIo(server);
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
